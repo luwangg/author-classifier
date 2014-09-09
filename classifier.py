058c16fbe86f4c7295b8d5686bac89c750b5ae82
Tim O'Shea <tim.oshea753@gmail.com>
2014-09-09 12:41:48 -0400

    classify at different L levels

--- classifier.py
+++ classifier.py
@@ -17,44 +17,58 @@ class CountVectorizer2(CountVectorizer):
         analyzer = CountVectorizer.build_analyzer(self)
         return lambda x: (stem.stem(y) for y in analyzer(x))
 
-# list of classes, file contents, and preamble delineators
-fulltext = {"Conan Doyle":("pg1661.txt","ADVENTURE I"), 
-            "Jane Austen":("pg31100.txt", "Chapter 1")};
-sent = {};
 
-# load books
-for k,v in fulltext.items():
-    # load the text and drop the preamble
-    fulltext[k] = open(v[0]).read().split(v[1],1)[1];
-    # split the text up into sentances
-    fulltext[k] = tokenizer.tokenize(fulltext[k]);
-    for c in fulltext[k]:
-        sent[c] = k;
+L = 10;
 
-# convert to indexed form
-count_vect = CountVectorizer2(lowercase=True, stop_words="english", strip_accents="ascii")
-#count_vect = CountVectorizer(lowercase=True, stop_words="english", strip_accents="ascii")
-a  = count_vect.fit_transform(sent.keys())
-target =  numpy.array(sent.values())
+for ll in range(0,L):
+    l = ll + 1;
 
-# set up clasifier
-from sklearn.naive_bayes import MultinomialNB
+    print "Showing results for classification using L=%d sentence examples"%(l)
 
-avals = {0:"ML",
-        0.5:"Good-Turing",
-        1:"Laplace",
-        2:"Two"};
+    # list of classes, file contents, and preamble delineators
+    fulltext = {"Conan Doyle":("pg1661.txt","ADVENTURE I"), 
+                "Jane Austen":("pg31100.txt", "Chapter 1")};
+    fulltextj = {};
+    sent = {};
 
-for alpha,aname in avals.items():
+    # load books
+    for k,v in fulltext.items():
+        # load the text and drop the preamble
+        fulltext[k] = open(v[0]).read().split(v[1],1)[1];
+        # split the text up into sentances
+        fulltext[k] = tokenizer.tokenize(fulltext[k]);
+        fulltextj[k] = [];
+
+        # join l sentances to make one test case
+        for i in range(0,len(fulltext[k])/l):
+            fulltextj[k].append( reduce( lambda x,y: x+y, fulltext[k][i*l:(i+1)*l] ) )
+                
+        for c in fulltextj[k]:
+            sent[c] = k;
+
+    # convert to indexed form
+    count_vect = CountVectorizer2(lowercase=True, stop_words="english", strip_accents="ascii")
+    a  = count_vect.fit_transform(sent.keys())
+    target =  numpy.array(sent.values())
     
-    clf = MultinomialNB(alpha=alpha)
-
-    # train and test classifier / cross validation
-    print "Cross validating NB classifier... (alpha=%f [%s])"%(alpha,aname)
-    scores = cross_validation.cross_val_score( clf, a, target, cv=10)
-    #scores = cross_validation.cross_val_score( clf, a, target, cv=5)
-    print scores
-    print("Accuracy: %0.2f (+/- %0.2f)  [Mis-classification = %0.2f]" % (scores.mean(), scores.std() * 2, 1-scores.mean()))
+    # set up clasifier
+    from sklearn.naive_bayes import MultinomialNB
+    
+    avals = {0:"ML",
+            0.5:"Good-Turing",
+            1:"Laplace",
+            2:"Two"};
+    
+    for alpha,aname in avals.items():
+        
+        clf = MultinomialNB(alpha=alpha)
+    
+        # train and test classifier / cross validation
+        print "Cross validating NB classifier... (alpha=%f [%s])"%(alpha,aname)
+        scores = cross_validation.cross_val_score( clf, a, target, cv=10)
+        #scores = cross_validation.cross_val_score( clf, a, target, cv=5)
+        print scores
+        print("Accuracy: %0.2f (+/- %0.2f)  [Mis-classification = %0.2f]" % (scores.mean(), scores.std() * 2, 1-scores.mean()))
 
 
 
