c0442ce6b4b9d6cff20981c03a6ddc39ea6ab76a
Tim O'Shea <tim.oshea753@gmail.com>
2014-09-14 20:02:09 -0400

    adding...

--- classifier.py
+++ classifier.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python
+#
+# Naive Bayes Author Classification from text
+#
+#  Copyright 2014 Tim O'Shea
+# 
+#  This classifier is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3, or (at your option)
+#  any later version.
+# 
+#  This software is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
 # http://www.nltk.org/
 # http://scikit-learn.org/stable/tutorial/text_analytics/working_with_text_data.html#tokenizing-text-with-scikit-learn
 # http://scikit-learn.org/stable/modules/cross_validation.html
@@ -17,9 +32,15 @@ class CountVectorizer2(CountVectorizer):
         analyzer = CountVectorizer.build_analyzer(self)
         return lambda x: (stem.stem(y) for y in analyzer(x))
 
-L = 10;
+L = 20;
+#Lr = range(0,L)
+Lr = [9];
+#L = 10;
 
-for ll in range(0,L):
+#for ll in range(0,L):
+
+
+for ll in Lr:
     l = ll + 1;
 
     print "Showing results for classification using L=%d sentence examples"%(l)
@@ -51,34 +72,40 @@ for ll in range(0,L):
     #count_vect = CountVectorizer2(lowercase=True, stop_words="english", strip_accents="ascii", min_df=2)
 
     # option two (cap max_features at 100)
-    count_vect = CountVectorizer2(lowercase=True, stop_words="english", strip_accents="ascii", min_df=5, max_features=100)
+    #count_vect = CountVectorizer2(lowercase=True, stop_words="english", strip_accents="ascii", min_df=5, max_features=500)
 
+    for F in range(1,100):
+    #for F in range(100,10100,100):
+#    for F in [100,200,300,400,500,600,700,800,900,1000,1500,2000]:
+        print "F = %d"%(F)
     #count_vect = CountVectorizer2(lowercase=True, stop_words="english", strip_accents="ascii", min_df=5)
     #count_vect = CountVectorizer2(lowercase=True, stop_words="english", strip_accents="ascii", min_df=1)
-    #count_vect = CountVectorizer2(lowercase=True, stop_words="english", strip_accents="ascii")
-    print count_vect
-    a  = count_vect.fit_transform(sent.keys())
-    print "Freq vect shape: " + str(a.shape)
-    target =  numpy.array(sent.values())
+        count_vect = CountVectorizer2(lowercase=True, stop_words="english", strip_accents="ascii", max_features=F)
+        #count_vect = CountVectorizer2(lowercase=True, stop_words="english", strip_accents="ascii", min_df=1)
+    #print count_vect
+        a  = count_vect.fit_transform(sent.keys())
+        print "Freq vect shape: " + str(a.shape)
+        target =  numpy.array(sent.values())
     
     # set up clasifier
-    from sklearn.naive_bayes import MultinomialNB
+        from sklearn.naive_bayes import MultinomialNB
     
-    avals = {0:"ML",
+        avals = {0:"ML",
             0.5:"Good-Turing",
             1:"Laplace",
             2:"Two"};
+        avals = {1:"Laplace"};
     
-    for alpha,aname in avals.items():
+        for alpha,aname in avals.items():
         
-        clf = MultinomialNB(alpha=alpha)
+            clf = MultinomialNB(alpha=alpha)
     
         # train and test classifier / cross validation
-        print "Cross validating NB classifier... (alpha=%f [%s])"%(alpha,aname)
-        scores = cross_validation.cross_val_score( clf, a, target, cv=10)
+            print "Cross validating NB classifier... (alpha=%f [%s])"%(alpha,aname)
+            scores = cross_validation.cross_val_score( clf, a, target, cv=10)
         #scores = cross_validation.cross_val_score( clf, a, target, cv=5)
-        print scores
-        print("Accuracy: %0.2f (+/- %0.2f)  [Mis-classification = %0.2f]" % (scores.mean(), scores.std() * 2, 1-scores.mean()))
+            print scores
+            print("Accuracy: %0.4f (+/- %0.4f)  [Mis-classification = %0.4f]" % (scores.mean(), scores.std() * 2, 1-scores.mean()))
 
 
 
